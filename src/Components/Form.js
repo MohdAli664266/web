0298f761517be460443ee39b932ae896676b118c
MohdAli664266 <moaliansari664266@gmail.com>
2023-12-27 09:38:34 +0530

    Fixed Issues of words

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,16 +2,8 @@ import React, {useState} from 'react'
 
 function Form(props){
     const [text, setText] = useState('')
-    let flag = false;
-    let words = text;
     const onChangeFunc = (event)=>{
         setText(event.target.value);
-        words = text;
-        if(words==='')
-        {
-            flag = true;
-        }else 
-            flag = false;
     }
     let prev= text;
     const onConvert = ()=>{
@@ -30,7 +22,7 @@ function Form(props){
                 <textarea className="form-control cl" onChange={onChangeFunc} value={text} id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
                 <button className="btn btn-danger my-2" onClick={onConvert}>Convert to upper</button>
-                <p><b>{words===flag?text.split(' ').length-1 : text.split(' ').length}</b> Words and <b>{text.length}</b> Characters</p>
+                <p><b>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</b>Words and <b>{text.length}</b> Characters</p>
             </div>
             <div className="container">
                 <h1>Prevew</h1>
